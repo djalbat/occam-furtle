55a1def0dc15382ca22715b074cdc746818bcdf5
James Smith <james.smith@djalbat.com>
2026-02-03 14:03:28 +0000

    Added an example application.

---
+++ src/example/view.js
@@ -0,0 +1,157 @@
+"use strict";
+
+import withStyle from "easy-with-style";  ///
+
+import { Element } from "easy";
+import { FurtleLexer,FurtleParser } from "occam-grammars";
+import { lexersUtilities, parsersUtilities } from "../index"; ///
+import { RowsDiv, ColumnDiv, ColumnsDiv, VerticalSplitterDiv } from "easy-layout";
+
+import SubHeading from "./view/subHeading";
+import SizeableDiv from "./view/div/sizeable";
+import BNFTextarea from "./view/textarea/bnf";
+import RuleNameInput from "./view/input/ruleName";
+import TokensTextarea from "./view/textarea/tokens";
+import ContentTextarea from "./view/textarea/content";
+import ParseTreeTextarea from "./view/textarea/parseTree";
+import LexicalEntriesTextarea from "./view/textarea/lexicalEntries";
+
+const { furtleParserFromBNF } = parsersUtilities,
+      { furtleLexerFromEntries } = lexersUtilities;
+
+const { bnf } = FurtleParser,
+      { entries } = FurtleLexer;
+
+class View extends Element {
+  keyUpHandler = (event, element) => {
+    // try {
+      const tokens = this.getTokens(),
+            parseTree = this.getParseTree(tokens);
+
+      this.setTokens(tokens);
+
+      this.setParseTree(parseTree);
+    // } catch (error) {
+    //   console.log(error);
+    //
+    //   this.clearTokens();
+    //
+    //   this.clearParseTree();
+    // }
+  }
+
+  getTokens() {
+    const lexicalEntries = this.getLexicalEntries(),
+          entries = lexicalEntries, ///
+          furtleLexer = furtleLexerFromEntries(entries),
+          lexer = furtleLexer,  ///
+          content = this.getContent(),
+          tokens = lexer.tokenise(content);
+
+    return tokens;
+  }
+
+  getParseTree(tokens) {
+    let parseTree = null;
+
+    const bnf = this.getBNF(),
+          furtleParser = furtleParserFromBNF(bnf),
+          parser = furtleParser,  ///
+          ruleName = this.getRuleName(),
+          ruleMap = parser.getRuleMap(),
+          rule = ruleMap[ruleName],
+          node = parser.parse(tokens, rule);
+
+    if (node !== null) {
+      parseTree = node.asParseTree(tokens);
+    }
+
+    return parseTree;
+  }
+
+  childElements() {
+    const { readOnly } = this.constructor;
+
+    return (
+
+      <ColumnsDiv>
+        <SizeableDiv>
+          <RowsDiv>
+            <SubHeading>
+              Lexical entries
+            </SubHeading>
+            <LexicalEntriesTextarea onKeyUp={this.keyUpHandler} readOnly={readOnly} />
+            <SubHeading>
+              BNF
+            </SubHeading>
+            <BNFTextarea onKeyUp={this.keyUpHandler} readOnly={readOnly} />
+            <SubHeading>
+              Rule name
+            </SubHeading>
+            <RuleNameInput onKeyUp={this.keyUpHandler} />
+          </RowsDiv>
+        </SizeableDiv>
+        <VerticalSplitterDiv />
+        <ColumnDiv>
+          <RowsDiv>
+            <SubHeading>
+              Content
+            </SubHeading>
+            <ContentTextarea onKeyUp={this.keyUpHandler} />
+            <SubHeading>
+              Tokens
+            </SubHeading>
+            <TokensTextarea />
+            <SubHeading>
+              Parse tree
+            </SubHeading>
+            <ParseTreeTextarea />
+          </RowsDiv>
+        </ColumnDiv>
+      </ColumnsDiv>
+
+    );
+  }
+
+  initialise() {
+    this.assignContext();
+
+    const { initialContent, initialRuleName } = this.constructor,
+          content = initialContent, ///
+          ruleName = initialRuleName, ///
+          lexicalEntries = entries; ///
+
+    this.setBNF(bnf);
+
+    this.setContent(content);
+
+    this.setRuleName(ruleName);
+
+    this.setLexicalEntries(lexicalEntries);
+
+    this.keyUpHandler();
+  }
+
+  static initialRuleName = null;
+
+  static initialContent = `Boolean isVariableFree(Node termNode, Node statementNode) {
+  Boolean variableBound = isVariableBound(termNode, statementNode);
+
+  Boolean variableFree = !variableBound;
+
+  return variableFree;
+}
+`;
+
+  static tagName = "div";
+
+  static defaultProperties = {
+    className: "view"
+  };
+}
+
+export default withStyle(View)`
+
+  padding: 1rem;
+ 
+`;
---
+++ src/example/view/div/sizeable.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import withStyle from "easy-with-style";  ///
+
+import { SizeableDiv } from "easy-layout";
+
+export default withStyle(SizeableDiv)`
+
+  width: 48rem;
+  min-width: 24rem;
+  
+`;
