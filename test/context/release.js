6534f512b759d125b6b7d0805e7c4701c50de7cc
James Smith <james.smith@djalbat.com>
2026-03-01 12:27:58 +0000

    Gotten the new Furtle file contexts working.

--- test/context/release.js
+++ test/context/release.js
@@ -3,9 +3,20 @@
 const { ReleaseContext : ReleaseContextBase } = require("occam-languages");
 
 class ReleaseContext extends ReleaseContextBase {
-  findFileContext() {
+  findFileContext(filePath) {
+    const fileContexts = this.getFileContexts(),
+          fileContext = fileContexts.find((fileContext) => {
+            const filePathMatches = fileContext.matchFilePath(filePath);
 
+            if (filePathMatches) {
+              return true;
+            }
+          }) || null;
+
+    return fileContext;
   }
+
+  static fromLogNameJSONEntriesCallbackAndCustomGrammar(log, name, json, entries, callback, customGrammar) { return ReleaseContextBase.fromLogNameJSONEntriesCallbackAndCustomGrammar(ReleaseContext, log, name, json, entries, callback, customGrammar); }
 }
 
 module.exports = ReleaseContext;
